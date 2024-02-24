0c65e1784d5a775de1577b67b22d89238ac2ca82
Felipe Alvarado <devfelipe663@gmail.com>
2024-02-23 21:27:50 -0500

    feat: add structure app

--- main.js
+++ main.js
@@ -1,7 +1,17 @@
 import './style.css'
 
 document.querySelector('#app').innerHTML = `
-  <div>
-    <h1>Hello Newsletter!</h1>
-  </div>
+  <section class="container">
+    <div class="newsletter">
+      <div class="newsletter__image">
+        <figure>
+          <img src="./assets/images/illustration-sign-up-mobile.svg">
+        </figure>
+      </div>
+
+      <div class="newsletter__form">
+        <h1>Hello Newsletter!</h1>
+      </div>
+    </div>
+  </section>
 `
