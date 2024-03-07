cf4cd45740cd3383bb29794b80b8b27662f6a5a2
Felipe Alvarado <devfelipe663@gmail.com>
2024-03-07 17:31:03 -0500

    feat: add titles and list items with styles

--- main.js
+++ main.js
@@ -10,7 +10,16 @@ document.querySelector('#app').innerHTML = `
       </div>
 
       <div class="newsletter__form">
-        <h1>Hello Newsletter!</h1>
+        <h1>Stay updated!</h1>
+        <p class="newsletter__form--desc">Join 60,000+ product managers receiving monthly updates on:</p>
+
+        <ul>
+          <li>Produc discovery and building what matters</li>
+          <li>Measuring to ensure updates are a success</li>
+          <li>And much more!</li>
+        </ul>
+
+
       </div>
     </div>
   </section>
