500c91f20165ae5337900d69ad8aff8593f31045
Felipe Alvarado <devfelipe663@gmail.com>
2025-01-29 20:06:07 -0500

    fix: changes for succes view

--- main.js
+++ main.js
@@ -5,17 +5,22 @@ document.addEventListener('DOMContentLoaded', () => {
   const app = document.querySelector('#app');
   app.appendChild(mainComponent());
 
+  const successViewElement = document.getElementById('success-view');
+
+  if (successViewElement) {
+    successViewElement.style.display = 'none';
+  }
 
   document.querySelector('form').addEventListener('submit', function (event) {
     event.preventDefault();
     document.getElementById('newsletter').style.display = 'none';
-    document.getElementById('thank-you-message').style.display = 'block';
-    let email = event.target.getElementById('email_in').value;
+    document.getElementById('success-view').style.display = 'block';
+    let email = document.getElementById('email_in').value;
     document.getElementById('email_span').innerText = email;
   });
 
   function dismissMessage() {
-    document.getElementById('thank-you-message').style.display = 'none';
+    document.getElementById('success-view').style.display = 'none';
     document.getElementById('newsletter').style.display = 'block';
   }
 
